a87352cfb82588b7e81f3b589798a285d7a18f3b
panda <expect_us@163.com>
2021-06-15 17:24:17 +0800

    合计组件

--- vue-react-webpack/src/components/count.vue
+++ vue-react-webpack/src/components/count.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <!-- <input type="text"  /> -->
+    <p>{{ data.a }}</p>
+    <input type="text" v-model="data.a" />
     <h3>{{ $store.state.count }}</h3>
     <button @click="$store.commit('add')">+</button>
     <button @click="$store.commit('reduce')">-</button>
@@ -9,7 +12,11 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      data: {
+        a: "pandaMr",
+      },
+    };
   },
 };
 </script>
